d3cc39b7cb920c836c6c4743a24f299012c23dc2
Akhil <akhiljith.c.a@gmail.com>
2021-06-20 00:38:53 +0530

    Add chat styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
+import {ChatEngine} from 'react-chat-engine';
+import './App.css';
 
 function App() {
   return (
-    <div >
- 
-    </div>
+     <ChatEngine 
+     height="100vh"
+     projectID="4671b462-5257-4b4b-ba7c-b4710d5b586c"
+     userName="jack"
+     userSecret="123"
+     />
   );
 }
 
