45f0c297b923ba9069287d9e179c80d13363a11b
Akhil <akhiljith.c.a@gmail.com>
2021-06-26 09:09:21 +0530

    Create Chat Feed
    Created jsx templates to show my messages and their messages.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {ChatEngine} from 'react-chat-engine';
 import './App.css';
+import ChatFeed from "./components/ChatFeed"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
      projectID="4671b462-5257-4b4b-ba7c-b4710d5b586c"
      userName="jack"
      userSecret="123"
+     renderChatFeed={(chatAppProps)=> <ChatFeed {...chatAppProps} />}
      />
   );
 }
