1cd295afa7406a9217cc7cf61a583502f4056c03
Akhil <akhiljith.c.a@gmail.com>
2021-06-26 14:38:46 +0530

    Pass props to child components from ChatFeed component

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -16,7 +16,7 @@ const ChatFeed = (props) => {
                     <div className="message-block">
                         isMyMessage? <MyMessage /> : <TheirMessage />
                     </div>
-                    <div className="read-receipts" style={{marginRight:isMyMessage?"18px":"0px", marginLeft:isMyMessage?"0px":"68px"}}>
+                    <div className="read-receipts" style={{ marginRight: isMyMessage ? "18px" : "0px", marginLeft: isMyMessage ? "0px" : "68px" }}>
 
                     </div>
                 </div>
@@ -26,7 +26,7 @@ const ChatFeed = (props) => {
         )
     }
 
-    if(!chat) return "Loading..."
+    if (!chat) return "Loading..."
     return (
         <div className="chat-feed">
             <div className="chat-title-container">
@@ -34,10 +34,14 @@ const ChatFeed = (props) => {
                     {chat.title}
                 </div>
                 <div className="chat-subtitle">
-                    {chat.people.map((person)=>'${person.person.username}')}
+                    {chat.people.map((person) => '${person.person.username}')}
                 </div>
             </div>
             {renderMessages()}
+            <div style={{ height: '100px' }} />
+            <div className="message-form-container">
+                <MessageForm  {...props} chatId={activeChat} />
+            </div>
         </div>
     )
 }
